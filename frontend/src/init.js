a67b4f083db3fa156a3883892e6d971514336cb1
TimChen014 <78790299+TimChen014@users.noreply.github.com>
2021-02-09 18:52:33 +0800

    finish my first MERN project

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,7 +1,3 @@
-// import React, { useEffect } from "react";
-// import patientsData from "./patients.json";
-// import ordersData from "./orders.json";
-
 const fetch = require('node-fetch');
 var fs = require('fs');
 let patientsData = fs.readFileSync('./patients.json');
@@ -10,15 +6,12 @@ let ordersData = fs.readFileSync('./orders.json');
 
 
 function componentDidMount(url, req) {
-    // Simple POST request with a JSON body using fetch
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req)
     };
     fetch(url, requestOptions)
-        // .then(response => response.json())
-        // .then(data => this.setState({ postId: data.id }));
 };
 
 patientsData = JSON.parse(patientsData);
